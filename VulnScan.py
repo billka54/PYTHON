5c03386ad943749d953539c638999a5032894153
Pol Pérez <124408377+billka54@users.noreply.github.com>
2023-03-28 20:12:11 +0200

    Delete VulnScan.py

--- VulnScan.py
+++
@@ -1,8 +0,0 @@
-import portscanner
-
-ip_target = input("Enter target IP: ")
-port = int(input("Enter an amount of ports you want to SCAN: "))
-vul_file = input("Enter to the file with vulnerable softwares: ")
-print("\n")
-
-
